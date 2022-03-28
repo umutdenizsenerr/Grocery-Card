8f2688c648587a74c49972ac4e5e25738afb4809
umutdenizsenerr <umutdeniz442010@gmail.com>
2022-03-28 10:56:59 +0300

    commit

--- GroceryCart/src/pages/WelcomePage/GroceryCart.js
+++ GroceryCart/src/pages/WelcomePage/GroceryCart.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "./GroceryCart.css";
+import Modal from "react-modal";
 
 const GroceryCart = (props) => {
   const [items, setItems] = useState([]);
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   useEffect(() => {}, [items]);
   const handleSubmit = (event) => {
@@ -11,6 +13,7 @@ const GroceryCart = (props) => {
     if (addedItem !== "") {
       setItems((items) => [...items, event.target[0].value]);
     }
+    setIsOpen(true);
   };
 
   const handleRemove = (event) => {
@@ -19,22 +22,40 @@ const GroceryCart = (props) => {
     setItems(newArr);
     console.log(items);
   };
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   return (
     <div>
       <div className="AddItems">
+        <link
+          href="https://fonts.googleapis.com/css?family=Lobster"
+          rel="stylesheet"
+          type="text/css"
+        />
         <img
           src="https://cdn.pixabay.com/photo/2013/07/12/14/53/cart-148964_960_720.png"
           height="50vh"
         />
+        <Modal className="ModalDesign" isOpen={modalIsOpen}>
+          <h4>Item {items[items.length - 1]} added successfully.</h4>
+          <button onClick={closeModal}>close</button>
+        </Modal>
         <form onSubmit={handleSubmit}>
           <label>
             <h5> Add Item</h5>
             <input type="text" />
           </label>
-          <button type="submit">+</button>
+          <button type="submit" className="Button">
+            +
+          </button>
         </form>
-        <h5>asd</h5>
+        <h3>Umut Market</h3>
       </div>
 
       <h5>Grocery Cart</h5>
