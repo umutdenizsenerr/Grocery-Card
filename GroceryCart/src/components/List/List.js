1ab3d2ac9860676bd7b3dc9b33b1349b3469c349
umutdenizsenerr <umutdeniz442010@gmail.com>
2022-03-29 15:12:56 +0300

    feat: List item changed

--- GroceryCart/src/components/List/List.js
+++ GroceryCart/src/components/List/List.js
@@ -1,44 +1,44 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import Button from "../Button/Button";
 import axios from "axios";
+import "./List.css";
 const List = (props) => {
-  let drinkNames = [];
+  const [drinkList, setDrinkList] = useState([]);
+  useEffect(() => {
+    getCocktailNames();
+  }, []);
+  /*  TODO grocerycartın içi
   const handleRemove = (event) => {
-    let newArr = [...props.items];
+    let newArr = [...drinkNames];
     newArr.splice(event.target.value, 1);
-    props.setItems(newArr);
-  };
-
+    setDrinkNames(newArr);
+  };*/
   const getCocktailNames = async () => {
     const base_url =
-      "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
-    let url = "";
-    for (let i = 97; i < 123; i++) {
-      url = base_url + String.fromCharCode(i);
-      await axios
-        .get(url)
-        .then((response) =>
-          response.data.drinks !== null
-            ? response.data.drinks.map((item) => drinkNames.push(item.strDrink))
-            : null
-        );
-    }
+      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic";
+
+    //TO DO then, catch  -> try, catch
+    await axios
+      .get(base_url)
+      .then((response) => setDrinkList(response.data.drinks))
+      .catch((error) => console.log(error));
   };
-  return (
-    <div>
-      <h5>Grocery Cart</h5>
-      <div className="ListContainer">
-        <ul>
-          {props.items.map((element, index) => (
-            <div className="Items">
-              <li key={index}>{element}</li>
-              <Button value={index} onClick={handleRemove} content="-" />
-            </div>
-          ))}
-        </ul>
-        <Button onClick={getCocktailNames} content="get data" />
+
+  const renderList = () => {
+    return drinkList.map((element) => (
+      <div className="list-item" key={element.idDrink}>
+        <div className="list-item-thumb-container">
+          <img className="list-item-thumb" src={element.strDrinkThumb} />
+        </div>
+
+        <div className="list-item-name">{element.strDrink}</div>
+        <div>
+          <Button type="icon" iconName="add" />
+          <Button type="icon" iconName="details" />
+        </div>
       </div>
-    </div>
-  );
+    ));
+  };
+  return <div className="list-container">{renderList()}</div>;
 };
 export default List;
